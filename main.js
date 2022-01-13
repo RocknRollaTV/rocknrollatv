b0b98eb47ba2c866db8e1dde4a2733278dd5e3cc
RocknRollaTV <97687583+RocknRollaTV@users.noreply.github.com>
2022-01-13 10:41:06 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var animalContainer = document.getElementById("animal-info");
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://richardeus1.github.io/jsonparser/animals.json');
+ourRequest.onload = function() {
+    var ourData = JSON.parse(ourRequest.responseText);
+    renderHTML(ourData);
+    };
+
+ourRequest.send();   
+
+function renderHTML(data){
+    var htmlString = "";
+    for (i= 0; i< data.length; i++){
+        htmlString += "<p>" + data[i].name + " is a " + data[i].species + " .</p>";
+    }
+    animalContainer.insertAdjacentHTML('beforeend', htmlString);
+}
